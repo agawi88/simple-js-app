aed8eda0a25a3dedfb80399ec3b02e54ace384d2
agawi88 <agnessn@icloud.com>
2024-09-08 22:51:15 +0200

    Update scripts.js
    added and array of objects. Each object has been assigned the same three keys with values.

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,1 +1,16 @@
-let pokemonList = [null];
+let pokemonList = ['mouse', 'siameseCat', 'fox'];
+let mouse = {
+    name: "Pikachu",
+    height: 0.4,
+    type: "electricShock", "lightningrod", "static",
+};
+let siameseCat = {
+    name: "Persian",
+    height: 1,
+    type: "haughty", "jewel", "limber",
+};
+let fox = {
+    name: "Vulpix",
+    height: 0.6,
+    type: "tails", "flashFire", "draught",
+}
