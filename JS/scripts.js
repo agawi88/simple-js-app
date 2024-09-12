2d0800492025d8f49240f281f7d613b830edf5bf
agawi88 <agnessn@icloud.com>
2024-09-12 12:40:53 +0200

    Update scripts.js
    removed indentures

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,18 +1,18 @@
 let pokemonList = [
-    {
-    name: "Pikachu",
-    height: 0.4,
-    types: ["mouse", "electricShock", "lightningrod", "static"],
-    },
-    {
-    name: "Persian",
-    height: 1,
-    types: ["siameseCat", "haughty", "jewel", "limber"],
-    },
-    {
-    name: "Vulpix",
-    height: 0.6,
-    types: ["fox", "tails", "flashFire", "draught"],
-    },
+{
+name: "Pikachu",
+height: 0.4,
+types: ["mouse", "electricShock", "lightningrod", "static"],
+},
+{
+name: "Persian",
+height: 1,
+types: ["siameseCat", "haughty", "jewel", "limber"],
+},
+{
+name: "Vulpix",
+height: 0.6,
+types: ["fox", "tails", "flashFire", "draught"],
+},
 ];
 
