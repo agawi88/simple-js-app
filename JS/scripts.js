5493323742ea100bab47f7f4c74a2f92971919f6
agawi88 <agnessn@icloud.com>
2024-09-12 12:38:52 +0200

    Update scripts.js
    added commas at the end of each type line and changed semicolons to commas between objects. Semicolon only at the end of the variable

--- JS/scripts.js
+++ JS/scripts.js
@@ -2,17 +2,17 @@ let pokemonList = [
     {
     name: "Pikachu",
     height: 0.4,
-    types: ["mouse", "electricShock", "lightningrod", "static"]
-    };
+    types: ["mouse", "electricShock", "lightningrod", "static"],
+    },
     {
     name: "Persian",
     height: 1,
-    types: ["siameseCat", "haughty", "jewel", "limber"]
-    };
+    types: ["siameseCat", "haughty", "jewel", "limber"],
+    },
     {
     name: "Vulpix",
     height: 0.6,
-    types: ["fox", "tails", "flashFire", "draught"]
-    }
+    types: ["fox", "tails", "flashFire", "draught"],
+    },
 ];
 
