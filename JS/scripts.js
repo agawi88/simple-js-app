1b2d5e09aedf090b98ac27d22c250eb5ecc2723c
agawi88 <agnessn@icloud.com>
2024-09-19 22:30:02 +0200

    Update scripts.js
    adjusted the visual and vocabulary aspect of the instructions in Part 3

--- JS/scripts.js
+++ JS/scripts.js
@@ -20,8 +20,8 @@ types: ["fox", "tails", "flashFire", "draught"],
 
 for (let i=0; i < pokemonList.length; i++) {
 if (pokemonList[i].height >= 1){
-console.log(pokemonList[i].name + pokemonList[i].height + ", wow! You're big!");
+console.log(pokemonList[i].name + " (height: "+ pokemonList[i].height + "), wow! You're big!");
 }else { 
-console.log(pokemonList[i].name + pokemonList[i].height + " you are tiny!"); 
+console.log(pokemonList[i].name + " (height: " + pokemonList[i].height + ") you are tiny!"); 
 }
 }
