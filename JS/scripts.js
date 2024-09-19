04cfdf69b2de38c423c8cb793633f6f7cfab3805
agawi88 <agnessn@icloud.com>
2024-09-19 22:53:24 +0200

    Update scripts.js
    testing solutions for Paart 1 of the exercise

--- JS/scripts.js
+++ JS/scripts.js
@@ -16,9 +16,16 @@ types: ["fox", "tails", "flashFire", "draught"],
 },
 ];
 
+let text = "";
+let i = 0;
+for (;pokemonList[i].name;){
+  text = text + " " + pokemonList[i].name;
+  i++;
+}
+document.write(text);
 
 
-for (let i=0; i < pokemonList.length; i++) {
+for (let i = 0; i < pokemonList.length; i++) {
 if (pokemonList[i].height >= 1){
 console.log(pokemonList[i].name + " (height: "+ pokemonList[i].height + "), wow! You're big!");
 }else { 
