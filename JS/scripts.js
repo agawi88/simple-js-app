1cfff888bbcb874e38c2f4b482c3fae2fb85c7fb
agawi88 <agnessn@icloud.com>
2024-10-21 17:26:58 +0200

    updated the app's js
    added new function showDetails, manipulated the DOM by adding list and buttons and an eventListener, updated the typeof

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,6 +1,6 @@
 var pokemonRepository = (function () {
 
-let pokemonList = [
+let repository = [
 {
 name: "Pikachu",
 height: 0.4,
@@ -19,38 +19,59 @@ types: ["fox", "tails", "flashFire", "draught"],
 ];
 
   function add(pokemon) {
-    if (typeof pokemon === 'object') {
-      pokemonList.push(pokemon);
+    if (
+      typeof pokemon === 'object' &&
+      "name" in pokemon &&
+      "height" in pokemon &&
+      "types" in pokemon) {
+      repository.push(pokemon);
+    }
+    else {
+      console.log("Pokemon is not correct");
     }
   }
 
   function getAll() {
-    return pokemonList;
+    return repository;
+  }
+
+  function addListItem(pokemon) {
+
+    let pokemonList = document.querySelector(".main_list") 
+    let ListItem = document.createElement("li");
+    let button = document.createElement("button");
+    button.innerText = pokemon.name;
+    button.classList.add("li_button");
+    ListItem.appendChild(button);
+    pokemonList.appendChild(ListItem);
+    button.addEventListener('click', function showDetails(pokemon) {
+      console.log(pokemon)
+    });
+  }
+
+  function showDetails(pokemon) {
+    console.log(pokemon)
   }
 
   return {
     add: add,
-    getAll: getAll
+    getAll: getAll,
+    addListItem: addListItem
   };
+
 })();
 
 console.log(pokemonRepository.getAll());
-pokemonRepository.add({ name: 'Pikachu' });
+pokemonRepository.add({ name: 'Pikachu', height: 0.3, types: ["electric"] });
 console.log(pokemonRepository.getAll()); 
 
-  function myLoopFunction(pokemon) {
-  
-   if (pokemon.height >= 1) {
-    document.write("<p><b>" + pokemon.name + " (height: " + pokemon.height + ")" + " Wow! You're big!" + "</b></p>");
-  } 
-  else {
-    document.write("<p>" + pokemon.name + " (height: " + pokemon.height + ")" + "You are tiny!" + "</p>");
-        }
-}
-pokemonRepository.getAll().forEach(myLoopFunction);
+pokemonRepository.getAll().forEach(function (pokemon) {
+  //console.log(pokemon); // is this necessary? it remain from the previous version of the forEach loop
+  pokemonRepository.addListItem(pokemon);
+});
 
 Object.keys(pokemonRepository.getAll()).forEach(function(property) {
-  document.write(property + ': ' + pokemonRepository.getAll()[property] + '<br>');
+  console.log(property + ': ' + pokemonRepository.getAll()[property] + '<br>');
 });
 
 function filterPokemon(arr, query) {
